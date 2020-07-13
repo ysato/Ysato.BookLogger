9cf20255aa1c565a0be8438cdb05dccb4bc76a4b
Yoshihiro <56924527+ysato@users.noreply.github.com>
2020-07-14 02:11:42 +0900

    refactor: getenv to $_ENV
    getenv returns string|false, phpstan angry X(

--- bin/setup.php
+++ bin/setup.php
@@ -6,16 +6,16 @@ require_once dirname(__DIR__) . '/autoload.php';
 
 use Symfony\Component\Dotenv\Dotenv;
 
-(new Dotenv())->usePutenv()->loadEnv(dirname(__DIR__) . '/.env');
+(new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
 
 chdir(dirname(__DIR__));
 passthru('rm -rf ./var/tmp/*');
 passthru('chmod 775 var/tmp');
 passthru('chmod 775 var/log');
 
-$dsn = sprintf('mysql:host=%s;port=%s;', getenv('DB_HOST'), getenv('DB_PORT'));
-$pdo = new \PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'));
-$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME'));
-$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME') . '_test');
+$dsn = sprintf('mysql:host=%s;port=%s;', $_ENV['DB_HOST'], $_ENV['DB_PORT']);
+$pdo = new \PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . $_ENV['DB_NAME']);
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . $_ENV['DB_NAME'] . '_test');
 passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e development');
 passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e test');
