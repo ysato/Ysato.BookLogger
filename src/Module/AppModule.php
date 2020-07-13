74fa09550bd7187a5b0a15beec99b5ef1ee03bb6
Yoshihiro <56924527+ysato@users.noreply.github.com>
2020-07-13 22:06:05 +0900

    chore: fix json schema link header

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -39,7 +39,7 @@ class AppModule extends AbstractAppModule
                 $appDir . '/var/json_validate',
             )
         );
-        $this->install(new JsonSchemaLinkHeaderModule('http://www.example.com/'));
+        $this->install(new JsonSchemaLinkHeaderModule('https://ysato.github.io/Ysato.BookLogger/schema/'));
         $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
         $this->install(new PackageModule());
     }
